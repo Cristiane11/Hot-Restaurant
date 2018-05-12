79b55854d887c1203ac6fd7ff45bcd103ca536d5
Nicole Gould <nicolegould@Nicoles-MacBook-Air.local>
2018-05-12 11:09:16 -0400

    js file

---
+++ hotrestaurant.js
@@ -0,0 +1,58 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+//New reservations
+var reservations = [
+    {
+        routeName: "Nicole",
+        name: "Nicole",
+        phoneNumber: 2033769156,
+        email: "123@yahoo.com",
+        uniqueID: 5
+    },
+    {
+        routeName: "Jocelyn",
+        name: "Jocelyn",
+        phoneNumber: 9735555555,
+        email: "jocelyn@gmail.com",
+        uniqueID: 4
+    }
+];
+
+
+// //Routes to home page
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "home.html"));
+// });
+
+// //Route to add reservation
+// app.get("/reservation", function(req, res){
+//     res.sendFile(path.join(__dirname, "add.html"));
+// });
+
+// //Displays all reservations
+// app.get("/api/reservations", function(req, res){
+//     return res.json(reservations);
+// });
+
+// //Displays a single reservation based on name or returns false
+// app.get("/api/reservations/:reservation", function(req, res){
+//     var chosen = req.params.reservation;
+
+//     console.log(chosen);
+
+// })
+
+app.listen(PORT, function(){
+    console.log("App is listening on PORT " + PORT);
+});
